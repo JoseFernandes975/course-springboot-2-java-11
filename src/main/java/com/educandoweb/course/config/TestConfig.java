e6167d48239cd40b4fd60fecd9e4494bb7c4661f
JoseFernandes975 <zehenrique752005@gmail.com>
2022-08-30 11:52:48 -0300

    Associação ManyToMany com JoinTable

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -53,6 +53,16 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		
+	   
 		Person u1 = new Person(null, "Maria Brown","maria@gmail.com","9888888","12345");
 		Person u2 = new Person(null, "Alex Green","alex@gmail.com","9777777","123456");
 		
